5962a8175338d8f80662cae991326d0dad2cdb21
mOHAMMEDaLREZQ <alrezq@vt.edu>
2024-06-04 11:50:47 -0400

    Kick off the session

---
+++ McNemars_test_1.R
@@ -0,0 +1,89 @@
+
+control <- read.csv(file="control.csv", header=TRUE, sep=",")
+
+treatment <- read.csv(file="treatment.csv", header=TRUE, sep=",")
+
+control = subset(control, select = -c(X))
+treatment =  subset(treatment, select = -c(X))
+control = subset(control, select = -c(shg_flag)) # remove treatment variable 
+treatment =  subset(treatment, select = -c(shg_flag)) # remove treatment variable
+#Read data files 
+
+t_G <- split(treatment, list( treatment$METHUSE,treatment$DIVISION )) 
+
+
+c_G <- split(control, list( control$METHUSE, control$DIVISION)) 
+
+
+
+tr_length=matrix( rep( 0, len=length(t_G)), nrow = 1);
+cr_length=matrix( rep( 0, len=length(t_G)), nrow = 1);
+
+# check which partition has how many samples
+
+for (i in 1:length(t_G)) {
+  tr_length[i]=nrow(t_G[[i]])
+  # nam_tr = paste("out_tr",i,sep="")
+  # assign(nam_tr, t_G[[i]]$change)
+  
+  cr_length[i]=nrow(c_G[[i]])  
+  # nam_cr = paste("out_cr",i,sep="")
+  # assign(nam_cr, c_G[[i]]$change)
+  
+}
+
+#creating the matrices to store the outputs
+tr_out=matrix( rep( 0), nrow = max(tr_length), ncol = length(t_G));
+cr_out=matrix( rep( 0), nrow = max(cr_length), ncol = length(c_G));
+
+#collecting the outputs for each partition in a matrix form
+for (i in 1:length(t_G)) {
+  if ( tr_length[i] == 0 | cr_length[i] == 0 ){
+    next
+  } else {
+    tr_out[1:tr_length[i],i]=t_G[[i]]$treatment_flag
+    cr_out[1:cr_length[i],i]=c_G[[i]]$treatment_flag
+  }
+}
+
+ #identifying the columns/partitions wghich has 0 entries/samples
+ L = c();
+ i = 1;
+ for ( i in 1:length(t_G)){
+   if ( tr_length[i] == 0 | cr_length[i] == 0 ){
+     b = i
+     L = c (L,b)
+     i = i+1
+   }
+ }
+ 
+ ## note, removed _f 
+ 
+# #eliminating the columns/partitions which has 0 entries/samples
+ tr_length_f = tr_length[, -L]
+ cr_length_f = cr_length[, -L]
+ 
+ tr_out_f = tr_out[, -L]
+ cr_out_f = cr_out[, -L]
+
+ ## note, removed _f because we dint run the above code because we set drop = true
+ 
+#writing into text and csv files
+write.table(tr_out_f, "tr_data.txt", sep="\t");
+write.table(tr_out_f, file="tr_data.csv",sep=",",row.names=F);
+
+
+write.table(cr_out_f, "cr_data.txt", sep="\t")
+write.table(cr_out_f, file="cr_data.csv",sep=",",row.names=F)
+
+
+
+write.table(tr_length_f, "tr_col_size.txt", sep="\t");
+write.table(tr_length_f, file="tr_col_size.csv",sep=",",row.names=F)
+
+write.table(cr_length_f, "cr_col_size.txt", sep="\t");
+write.table(cr_length_f, file="cr_col_size.csv",sep=",",row.names=F)
+
+
+
+##################################################################################################
---
+++ OAENET.R
@@ -0,0 +1,129 @@
+########################################
+library(msaenet)
+library(glmnet)
+library(MatchIt)
+library(lmtest)
+library(phonTools)
+library(MASS)
+library(sandwich)
+library(CovSel)
+#install.packages("mlbench")
+library(mlbench)
+#library(rJava)
+#library(CovSelHigh)
+library(mltools)
+library(data.table)
+library("matrixStats")
+library(dplyr)
+#install.packages("dplyr")
+library(dplyr)
+library(tictoc)
+# install.packages("boot")
+library(boot)
+########################################################
+
+################################
+#Processing data
+
+raw_data = read.csv("Statsig.csv")
+raw_data = subset(raw_data, select = -c(X) )
+
+#####################################
+
+itr_max = 1
+
+max_var = 1
+
+# #declare a zero matrix to hold att
+att.onet = zeros(itr_max)
+
+# #declare zero matrices to store the indexes of selected variables
+select.var.list.onet = zeros(itr_max, max_var)
+
+# #Declare zero matrices to store the total number of variables selected
+total.num.var.onet = zeros(itr_max)
+
+# #Declare zero matrices to store the total number of variables selected
+time.onet = zeros(itr_max)
+
+
+#########
+
+#will have to change that in the future
+
+for (i in 1:1){
+  itr = 1
+
+########################
+# Preparing the covariates, outcomes, and treat data frame
+
+df = raw_data
+Y = df$treatment_flag #enter your outcome variable 
+A = df$shg_flag #enter your treatment variable 
+
+############################
+
+XA = subset(df, select = -c(treatment_flag)) #outcome
+XX = subset(XA, select = -c(shg_flag)) #treatment
+
+
+############################
+# Applying Outcome adaptive elastic net
+# Step 1: Fitting Outcome Model Logistic regression for penalty
+
+my_logit_mod = glm(treatment_flag ~ ., data = df, family = "binomial")
+odds_ratio = (coef(my_logit_mod))
+odds_ratio
+odds_ratio = odds_ratio[-c(1,ncol(df))] # remove odds ratio for intercept and udpypnr  #df <- subset(df, select = -c(a, c))
+
+odds_ratio
+
+# Setting the penalty
+gamma = 5
+
+penalty = 1/(abs(odds_ratio))^gamma
+
+penalty[is.na(penalty)] <- 10000000000 # large penalty
+
+#Perform cross validation
+tic()
+
+K.cv.fit = cv.glmnet(data.matrix(XX), data.matrix(A),  type.measure = "mse",
+                     nfolds = 5, gamma = c(0, 0.25, 0.5, 0.75, 1), relax = TRUE, family="binomial") #check
+
+cv.alpha = K.cv.fit$relaxed
+alpha.opt = cv.alpha$gamma.1se
+
+#Step 2: Fitting Treatment Model 
+fit2=glmnet(as.matrix(XX), as.matrix(A), alpha =alpha.opt, lambda = K.cv.fit$lambda.1se, penalty.factor = penalty , family="binomial")
+
+t=toc()
+time.onet [itr]= t$toc - t$tic
+
+beta_ps_raw = as.matrix(coef(fit2))
+beta_ps_raw = as.matrix(coef(fit2, s = 0.02)) # check this 
+beta_ps_allvar = as.matrix(beta_ps_raw[2:(nrow(beta_ps_raw))]) #remove intercept to the last item
+
+beta_ps_non_zero = row(beta_ps_allvar)[which(abs(beta_ps_allvar) >= 0.1)] # check
+
+
+xx_true = XX[, beta_ps_non_zero]
+
+#Variables selected by OAENet
+onet.true.var.list = names(xx_true)
+treat.form = formula(paste("treatment_flag~",paste(onet.true.var.list,collapse="+")))
+
+## performing matching using Nearest Neighbor Matching 
+mm = matchit(treat.form, data = df, method = "nearest", distance = "glm", ratio = 1, caliper = .25, link = "logit", estimand = "ATT", replace = FALSE, discard = "control")
+
+#Calculating Treatment Effect
+effect.form = formula(paste("treatment_flag ~ shg_flag+",paste(onet.true.var.list,collapse="+")))
+fit.effect = lm(effect.form, data = match.data(mm))
+final.coef = fit.effect$coefficients
+att.onet[itr] = final.coef[2]
+
+print("OAEnet")
+print(att.onet[itr])
+print("Variables selected by OAENet")
+print(onet.true.var.list)
+}
